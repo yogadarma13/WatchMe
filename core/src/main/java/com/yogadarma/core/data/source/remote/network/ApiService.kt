6eac3803512d312dc5fcce622dfcc8512ef16e81
Yoga Darma <kadekyoga125@gmail.com>
2020-12-23 18:02:29 +0800

    remove file constans

--- core/src/main/java/com/yogadarma/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/yogadarma/core/data/source/remote/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.yogadarma.core.data.source.remote.network
 
+import com.yogadarma.core.BuildConfig
 import com.yogadarma.core.data.source.remote.response.DetailMovieResponse
 import com.yogadarma.core.data.source.remote.response.DetailTVShowResponse
 import com.yogadarma.core.data.source.remote.response.ListMovieResponse
@@ -12,33 +13,33 @@ interface ApiService {
 
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovie(
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): ListMovieResponse
 
     @GET("movie/popular")
     suspend fun getPopularMovie(
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): ListMovieResponse
 
     @GET("tv/airing_today")
     suspend fun getNowPlayingTVShow(
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): ListTVShowResponse
 
     @GET("tv/popular")
     suspend fun getPopularTVShow(
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): ListTVShowResponse
 
     @GET("movie/{id}")
     suspend fun getDetailMovie(
         @Path("id") id: Int,
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): DetailMovieResponse
 
     @GET("tv/{id}")
     suspend fun getDetailTVShow(
         @Path("id") id: Int,
-        @Query("api_key") apiKey: String = Constant.API_KEY
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
     ): DetailTVShowResponse
 }
